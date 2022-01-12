68639bddfda4f7aa41328018423e8acd2469bf35
mma <mma@rossvideo.com>
2022-01-11 21:16:30 -0500

    set up code kata repo

---
+++ src/main/java/org/echocat/kata/java/part1/MainApp.java
@@ -0,0 +1,13 @@
+package org.echocat.kata.java.part1;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println(getHelloWorldText());
+    }
+
+    protected static String getHelloWorldText() {
+        return "Hello world!";
+    }
+
+}
